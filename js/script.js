0e5a5a4da07ece5f373d72afea9ec98d5c5da811
dmorales1414 <dmorales@student.neumont.edu>
2026-02-25 16:11:46 -0700

    Added more context

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const music = document.getElementById("background-music");
 const toggleBtn = document.getElementById("audio-toggle");
 
 toggleBtn.addEventListener("click", () => {
+    // Toggle play/pause and update button text
+    // When paused, will be paused indefenintely until user clicks again
     if (music.paused) {
         music.play();
         toggleBtn.textContent = "Mute";
